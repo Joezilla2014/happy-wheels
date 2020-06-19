29d4d7779a098787b2aca649654088403a86ceb2
Joezilla2014 <63375064+Joezilla2014@users.noreply.github.com>
2020-06-19 13:26:30 -0400

    Update use.js

--- use.js
+++ use.js
@@ -2,4 +2,6 @@ const HappyWheels = requirejs(['happy-wheels/HappyWheels'], function(HappyWheels
 
 HappyWheels.searchLevels('rope swing', 'rating').then(([ level ]) => {
     console.log(level);
-});
+};
+};
+);
